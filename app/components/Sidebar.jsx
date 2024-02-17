ae5e26d0b7f5d410a47a3bb5e24157a2ba975462
Sarthak Sharma <sarthak2232@gmail.com>
2024-02-18 04:42:30 +0530

    detailing in sidebar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -6,27 +6,30 @@ import { FiFileText } from "react-icons/fi";
 import { RxPerson } from "react-icons/rx";
 import { TbReceipt } from "react-icons/tb";
 import { LiaStampSolid } from "react-icons/lia";
-import { CiLocationArrow1 } from "react-icons/ci";
-import { SlEnvolopeLetter } from "react-icons/sl";
+import { BsCursor } from "react-icons/bs";
+import { BsEnvelopePaper } from "react-icons/bs";
 import { BsJournalBookmark } from "react-icons/bs";
 import { IoLogoGooglePlaystore } from "react-icons/io5";
 import { FaApple } from "react-icons/fa";
 import { useState } from "react";
+import SidebarOption from "./SidebarOption";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
+  const [clickedOption, setClickedOption] = useState("");
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
-  const toggleClick = () => {
-    setIsClicked(true);
+  const toggleClick = (value) => {
+    setClickedOption(value);
   };
 
   return (
     <div
-      className="flex flex-col bg-[#161D41] h-full hover:w-64  w-12 justify-between py-1 font-normal text-base z-10 transition-all duration-300 text-nowrap"
+      className={`flex flex-col bg-[#161D41] h-full ${
+        isOpen ? "w-64" : "w-12"
+      }  justify-between py-1 font-normal text-base z-10 transition-all duration-300 text-nowrap`}
       onMouseEnter={toggleSidebar}
       onMouseLeave={toggleSidebar}
     >
@@ -37,95 +40,154 @@ const Sidebar = () => {
           <img
             src="https://x.razorpay.com/dist/assets/img/RazorpayX-logo.svg"
             alt="logo"
-            className="w-40 h-8 mx-4 my-4"
+            className="w-40 h-8 mx-4 my-4 px-1"
           />
         ) : (
-          <div>
-            <img
-              src="https://x.razorpay.com/dist/assets/img/x-logo.svg"
-              alt="logoX"
-              className="mx-2 my-4 h-8"
-            />
-          </div>
+          <img
+            src="https://x.razorpay.com/dist/assets/img/x-logo.svg"
+            alt="logoX"
+            className="mx-2 my-4 h-8 px-1"
+          />
         )}
 
         {/* section 1 */}
         <div className="py-1">
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            {/* {!isClicked && (
-              <div
-                className="bg-yellow-600 rounded-sm h-5 w-1 p
-              ml-[-12px] mr-[9px]"
-              ></div>
-            )} */}
-            <div className="pb-1">
-              <FiHome />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Home</div>}
+          <div onClick={() => toggleClick("Home")}>
+            <SidebarOption
+              name="Home"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Home" ? true : false}
+              icon={
+                <FiHome
+                  style={{
+                    strokeWidth: clickedOption === "Home" ? 3 : 2,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <GoArrowUpRight />
-            </div>
-            {isOpen && (
-              <div className="flex">
-                <div className="ml-3 text-sm">Payouts</div>
-                <div className="rounded-xl bg-green-700 text-xs text-white px-2 mx-3 self-center">
-                  NEW
-                </div>
-              </div>
-            )}
+          <div onClick={() => toggleClick("Payouts")}>
+            <SidebarOption
+              name="Payouts"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Payouts" ? true : false}
+              icon={
+                <GoArrowUpRight
+                  style={{
+                    strokeWidth: clickedOption === "Payouts" ? 2 : 1,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <FiFileText />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Account Statement</div>}
+          <div onClick={() => toggleClick("Account Statement")}>
+            <SidebarOption
+              name="Account Statement"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Account Statement" ? true : false}
+              icon={
+                <FiFileText
+                  style={{
+                    strokeWidth: clickedOption === "Account Statement" ? 3 : 2,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <RxPerson />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Contacts</div>}
+          <div onClick={() => toggleClick("Contacts")}>
+            <SidebarOption
+              name="Contacts"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Contacts" ? true : false}
+              icon={
+                <RxPerson
+                  style={{
+                    strokeWidth: clickedOption === "Contacts" ? 1 : 0,
+                  }}
+                />
+              }
+            />
           </div>
         </div>
         {/* section 2 */}
         <div className="pt-1 border-t border-[#1D2345] pb-32">
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <TbReceipt />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Vendor Payments</div>}
+          <div onClick={() => toggleClick("Vendor Payments")}>
+            <SidebarOption
+              name="Vendor Payments"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Vendor Payments" ? true : false}
+              icon={
+                <TbReceipt
+                  style={{
+                    strokeWidth: clickedOption === "Vendor Payments" ? 3 : 2,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <LiaStampSolid />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Tax Payments</div>}
+          <div onClick={() => toggleClick("Tax Payments")}>
+            <SidebarOption
+              name="Tax Payments"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Tax Payments" ? true : false}
+              icon={
+                <LiaStampSolid
+                  style={{
+                    strokeWidth: clickedOption === "Tax Payments" ? 2 : 1,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <CiLocationArrow1 />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Payout Links</div>}
+          <div onClick={() => toggleClick("Payout Links")}>
+            <SidebarOption
+              name="Payout Links"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Payout Links" ? true : false}
+              icon={
+                <BsCursor
+                  style={{
+                    strokeWidth: clickedOption === "Payout Links" ? 1 : 0,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <SlEnvolopeLetter />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Payroll</div>}
+          <div onClick={() => toggleClick("Payroll")}>
+            <SidebarOption
+              name="Payroll"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Payroll" ? true : false}
+              icon={
+                <BsEnvelopePaper
+                  style={{
+                    strokeWidth: clickedOption === "Payroll" ? 1 : 0,
+                  }}
+                />
+              }
+            />
           </div>
-          <div className="flex flex-row my-4 p-3  hover:bg-[#202749]">
-            <div className="pb-1">
-              <BsJournalBookmark />
-            </div>
-            {isOpen && <div className="ml-3 text-sm">Reports</div>}
+          <div onClick={() => toggleClick("Reports")}>
+            <SidebarOption
+              name="Reports"
+              isOpen={isOpen}
+              isClicked={clickedOption === "Reports" ? true : false}
+              icon={
+                <BsJournalBookmark
+                  style={{
+                    strokeWidth: clickedOption === "Reports" ? 1 : 0,
+                  }}
+                />
+              }
+            />
           </div>
         </div>
       </div>
       {/* app link */}
       {isOpen && (
-        <div className="flex flex-row justify-evenly p-3 text-sm fixed bottom-0 bg-[#161D41] w-64 transition-all duration-300">
+        <div
+          className={`flex grow flex-row justify-evenly p-3 text-sm fixed bottom-0 bg-[#161D41] transition-all duration-300`}
+        >
           Get RazorpayX mobile app
           <div className="ml-2">
             <FaApple />
---
+++ app/components/SidebarOption.jsx
@@ -0,0 +1,55 @@
+"use client";
+import { IoMdAdd } from "react-icons/io";
+import { useState } from "react";
+
+const SidebarOption = ({ name, isOpen, icon, isClicked }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const toggleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
+  return (
+    <div
+      className={`flex flex-row my-4 py-2 pl-3 ${
+        isClicked && isOpen
+          ? "bg-[#2B3252] text-white font-semibold hover:bg-[#2B3252]"
+          : "hover:bg-[#202749]"
+      } ${isClicked && "pl-2"}`}
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}
+    >
+      <div className="flex items-center justify-center">
+        {isClicked ? (
+          <div className="bg-yellow-600 rounded-r-md h-6 w-1 ml-[-8px] mr-2"></div>
+        ) : null}
+        <span
+          className={`px-1 py-1 ${
+            isClicked && !isOpen ? "bg-[#2B3252] rounded-md" : null
+          }`}
+        >
+          {icon}
+        </span>
+        {isOpen && (
+          <div className="flex items-center w-[220px]">
+            <div className="ml-2 text-sm">{name}</div>
+            {name === "Payouts" ? (
+              <div className="rounded-xl bg-green-700 text-xs text-white px-2 mx-3 self-center">
+                NEW
+              </div>
+            ) : null}
+            {isHovered &&
+            (name === "Payouts" ||
+              name === "Contacts" ||
+              name === "Vendor Payments") ? (
+              <div className="flex p-2 my-[-8px] text-lg bg-[#353B5A] rounded-sm ml-auto hover:bg-[#494F6A]">
+                <IoMdAdd />
+              </div>
+            ) : null}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SidebarOption;
